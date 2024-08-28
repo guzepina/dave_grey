ce893695a52edc04d4596794708ac4e2f32da6e9
Nexy <nemanja1991alucard@gmail.com>
2024-08-28 18:04:46 +0200

    Your descriptive commit message here

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,3 +27,5 @@ urlpatterns = [
     path('about/',views.about ),
     path('posts/', include('posts.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Posts
 
+
 # Create your views here.
 def posts_list(request):
     posts = Posts.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', { 'posts': posts})
+
+def post_page(request, slug):
+    posts = Posts.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', { 'post': posts})
 
