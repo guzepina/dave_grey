6af23775a5a570b040ca1f1ad8d719b30072ddc3
Nexy <nemanja1991alucard@gmail.com>
2024-08-31 15:51:48 +0200

    updatet new

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ app_name='users'
 
 urlpatterns = [
     
-    path('register/',views.register_view, name="register" )
+    path('register/',views.register_view, name="register" ),
+    path('login/',views.login_view, name="login" )
     
 ]
